7e93bf3f44da91855532f356a536b76c435b9c52
s.nemchuk <s.nemchuk@sdventures.com>
2020-01-17 10:44:47 +0300

    Первый тест

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Tests
 {
@@ -12,7 +14,19 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.That(1 > 2, Is.True, "Неверный hgtpekmfndfs");
+            IWebDriver browser = new ChromeDriver();
+
+            browser.Navigate().GoToUrl("https://www.dating.com/");
+            string etalonSlogan = "Your World. Your Love.";
+
+
+            string realSlogan = browser.FindElement(By.CssSelector("h1  l10n")).Text;
+
+            Assert.That(browser.Title, Does.Contain("Dating"), "Кривой title");
+            Assert.That(etalonSlogan, Is.EqualTo(realSlogan), "Кривой слоган");
+
         }
+
+      
     }
 }
